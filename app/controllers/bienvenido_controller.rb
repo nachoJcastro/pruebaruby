a56843a7a9c621940455733fe58ae4e40ad9e183
nachojcastro <nachoj.castro@gmail.com>
2016-07-21 17:52:26 +0000

    vistas nuevas nav

---
+++ app/controllers/bienvenido_controller.rb
@@ -0,0 +1,13 @@
+class BienvenidoController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def contacto
+  end
+
+  def faq
+  end
+end
---
+++ test/controllers/bienvenido_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class BienvenidoControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+  test "should get contacto" do
+    get :contacto
+    assert_response :success
+  end
+
+  test "should get faq" do
+    get :faq
+    assert_response :success
+  end
+
+end
