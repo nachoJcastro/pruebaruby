cbf5db37040869cc2a05823bc889044cb4b7ff52
nachojcastro <nachoj.castro@gmail.com>
2016-07-22 18:32:13 +0000

    coleccion

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160722172352) do
+ActiveRecord::Schema.define(version: 20160722175527) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
     t.string   "manager"
     t.string   "status"
     t.integer  "terms"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "employees", force: :cascade do |t|
+    t.string   "name"
+    t.string   "phone"
+    t.string   "job_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
