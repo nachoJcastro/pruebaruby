1f29055cdb921287d2785b2c62700aeca8de6094
nachojcastro <nachoj.castro@gmail.com>
2016-07-22 17:52:01 +0000

    csv-2

--- app/views/invoices/_invoice.json.jbuilder
+++ app/views/invoices/_invoice.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! invoice, :id, :date, :company, :tax, :salesperson, :created_at, :updated_at
+json.extract! invoice, :id, :date, :company, :tax, :salesperson, :status_type, :created_at, :updated_at
 json.url invoice_url(invoice, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160722004127) do
+ActiveRecord::Schema.define(version: 20160722172352) do
+
+  create_table "companies", force: :cascade do |t|
+    t.string   "name"
+    t.string   "manager"
+    t.string   "status"
+    t.integer  "terms"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "invoices", force: :cascade do |t|
     t.datetime "date"
@@ -20,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160722004127) do
     t.string   "salesperson"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "status_type"
   end
 
   create_table "users", force: :cascade do |t|
